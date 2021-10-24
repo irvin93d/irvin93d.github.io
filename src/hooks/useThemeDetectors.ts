af9bc49384da6e658e6e28c0e51ce4f8607f94c9
Duane Irvin <irvin93d@gmail.com>
2021-10-24 18:33:57 +0200

    Default to dark theme if preferred not set

--- src/hooks/useThemeDetectors.ts
+++ src/hooks/useThemeDetectors.ts
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react'
 
 const useThemeDetector = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(
-    window.matchMedia('(prefers-color-scheme: dark)').matches,
+    !window.matchMedia('(prefers-color-scheme: light)').matches,
   )
 
   useEffect(() => {
-    const mqListener = ({ matches }: MediaQueryListEvent) => setIsDarkTheme(matches)
-    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+    const mqListener = ({ matches }: MediaQueryListEvent) => setIsDarkTheme(!matches)
+    const darkThemeMq = window.matchMedia('(prefers-color-scheme: light)')
 
     darkThemeMq.addEventListener('change', mqListener)
     return () => darkThemeMq.removeEventListener('change', mqListener)
